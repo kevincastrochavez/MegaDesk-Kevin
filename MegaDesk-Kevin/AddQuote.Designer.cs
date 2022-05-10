5b918fc805ea73125909e883b6563a1ce15a8fe0
kevincastrochavez <cas20057@byui.edu>
2022-05-09 20:00:02 -0600

    Included all ui components for the add quote window and some basic rules for those

--- MegaDesk-Kevin/AddQuote.Designer.cs
+++ MegaDesk-Kevin/AddQuote.Designer.cs
@@ -30,22 +30,39 @@
         {
             this.btnSaveAdd = new System.Windows.Forms.Button();
             this.btnCancelAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.customerName = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.width = new System.Windows.Forms.NumericUpDown();
+            this.depth = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.surface = new System.Windows.Forms.ComboBox();
+            this.drawers = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.rush = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.width)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.depth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.drawers)).BeginInit();
             this.SuspendLayout();
             // 
             // btnSaveAdd
             // 
             this.btnSaveAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSaveAdd.Location = new System.Drawing.Point(217, 634);
+            this.btnSaveAdd.Location = new System.Drawing.Point(213, 694);
             this.btnSaveAdd.Name = "btnSaveAdd";
             this.btnSaveAdd.Size = new System.Drawing.Size(329, 123);
             this.btnSaveAdd.TabIndex = 0;
-            this.btnSaveAdd.Text = "Save";
+            this.btnSaveAdd.Text = "Add Quote";
             this.btnSaveAdd.UseVisualStyleBackColor = true;
             // 
             // btnCancelAdd
             // 
             this.btnCancelAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancelAdd.Location = new System.Drawing.Point(679, 634);
+            this.btnCancelAdd.Location = new System.Drawing.Point(671, 694);
             this.btnCancelAdd.Name = "btnCancelAdd";
             this.btnCancelAdd.Size = new System.Drawing.Size(329, 123);
             this.btnCancelAdd.TabIndex = 1;
@@ -53,17 +70,199 @@
             this.btnCancelAdd.UseVisualStyleBackColor = true;
             this.btnCancelAdd.Click += new System.EventHandler(this.btnCancel_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(79, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Customer Name:";
+            // 
+            // customerName
+            // 
+            this.customerName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customerName.Location = new System.Drawing.Point(361, 75);
+            this.customerName.Margin = new System.Windows.Forms.Padding(10);
+            this.customerName.Name = "customerName";
+            this.customerName.Size = new System.Drawing.Size(376, 50);
+            this.customerName.TabIndex = 3;
+            this.customerName.Text = "Customer Name";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rush);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.drawers);
+            this.groupBox1.Controls.Add(this.surface);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.depth);
+            this.groupBox1.Controls.Add(this.width);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(86, 208);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1092, 412);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Desk Info";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 37);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Width (in):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 37);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Depth (in):";
+            // 
+            // width
+            // 
+            this.width.Location = new System.Drawing.Point(201, 90);
+            this.width.Maximum = new decimal(new int[] {
+            96,
+            0,
+            0,
+            0});
+            this.width.Minimum = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            this.width.Name = "width";
+            this.width.Size = new System.Drawing.Size(132, 44);
+            this.width.TabIndex = 2;
+            this.width.Value = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            // 
+            // depth
+            // 
+            this.depth.Location = new System.Drawing.Point(201, 217);
+            this.depth.Maximum = new decimal(new int[] {
+            48,
+            0,
+            0,
+            0});
+            this.depth.Minimum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.depth.Name = "depth";
+            this.depth.Size = new System.Drawing.Size(132, 44);
+            this.depth.TabIndex = 3;
+            this.depth.Value = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(578, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(305, 37);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Number of Drawers:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(519, 219);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(258, 37);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Surface Material:";
+            // 
+            // surface
+            // 
+            this.surface.FormattingEnabled = true;
+            this.surface.Items.AddRange(new object[] {
+            "Laminate",
+            "Oak",
+            "Rosewood",
+            "Veneer",
+            "Pine"});
+            this.surface.Location = new System.Drawing.Point(797, 216);
+            this.surface.Name = "surface";
+            this.surface.Size = new System.Drawing.Size(253, 45);
+            this.surface.TabIndex = 6;
+            // 
+            // drawers
+            // 
+            this.drawers.Location = new System.Drawing.Point(918, 97);
+            this.drawers.Maximum = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+            this.drawers.Name = "drawers";
+            this.drawers.Size = new System.Drawing.Size(132, 44);
+            this.drawers.TabIndex = 7;
+            this.drawers.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(519, 344);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(191, 37);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Rush Order:";
+            // 
+            // rush
+            // 
+            this.rush.FormattingEnabled = true;
+            this.rush.Items.AddRange(new object[] {
+            "No rush",
+            "3 days",
+            "5 days",
+            "7 days"});
+            this.rush.Location = new System.Drawing.Point(797, 341);
+            this.rush.Name = "rush";
+            this.rush.Size = new System.Drawing.Size(253, 45);
+            this.rush.TabIndex = 9;
+            // 
             // AddQuote
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1274, 829);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.customerName);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnCancelAdd);
             this.Controls.Add(this.btnSaveAdd);
             this.Name = "AddQuote";
             this.Text = "AddQuote";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.AddQuote_FormClosed);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.width)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.depth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.drawers)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,5 +270,18 @@
 
         private System.Windows.Forms.Button btnSaveAdd;
         private System.Windows.Forms.Button btnCancelAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox customerName;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown depth;
+        private System.Windows.Forms.NumericUpDown width;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox surface;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox rush;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown drawers;
     }
 }
